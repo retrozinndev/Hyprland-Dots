6a6c0449e613a43912a1137cb7789eb8ee7e3e06
João Dias <joaovodias@gmail.com>
2024-07-26 19:03:29 -0300

    :wrench: chore(apply.sh): fixed functions and function calls

--- apply.sh
+++ apply.sh
@@ -7,40 +7,43 @@ echo "######################################";
 printf "\n";
 
 
-echo "Welcome to my dotfiles installation script!";
+echo "Welcome to my dotfiles installation script!"
 
 # Say that dotfiles aren't ready to use yet and exit
-echo "Oh oh! Looks like my Dotfiles aren't ready to use yet! Try again later... exiting.";
-printf "\n";
-exit 0;
+echo "Oh oh! Looks like my Dotfiles aren't ready to use yet! Try again later... exiting."
+printf "\n"
+exit 0
 
-
-echo -n "Do you want to install the dotfiles? [y/n] ";
-read input;
-
-if ($input == "y" || $input == "yes")
-then
-	install()
-else
-	exit 0
-fi
-
-function install() {
-	
-	printf "\n";
-	echo "Making backup before installing dotfiles...";
+function backup_previous_dotfiles {
+    echo "Making backup before installing dotfiles..."
 	# Do backup steps here
-	echo "Finished backup.";
-	
-	printf "\n";
+	echo "Finished backup."
+}
 
-	echo "Starting dotfiles installation...";
-	echo "[info] Installing waybar theme";
+function apply_dotfiles {
+	
+	printf "\n"
+    backup_previous_dotfiles
+	printf "\n"
+
+	printf "Starting dotfiles installation...\n"
+
 	echo "[info] Installing Hyprland configs"
 	echo "[info] Installing wofi stylesheet"
 	echo "Ah yes! Looks like it's ready to use! If you find any issue, report at: https://github.com/retrozinndev/Hyprland-Dots/issues "
-	echo "Thanks for using my dotfiles! I'm really happy for that :3"
+	echo "Thanks for using my dotfiles! I'm really happy with that :3"
 }
+
+echo -n "Do you want to install the dotfiles? [y/n] "
+read input
+
+if [[ $input = "y" ]] || [[ $input = "yes" ]]
+then
+	apply_dotfiles
+else
+    printf "Ok, doing as you said! Bye bye!\n"
+	exit 0
+fi
 
 printf "\n"
 
