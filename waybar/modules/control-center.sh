8f2e4c2dc7ff08b697b4ab44c51bbe28a3ac0855
retrozinndev <joaovodias@gmail.com>
2024-10-21 21:05:04 -0300

    :recycle: refactor(waybar): separate modules and general styling to other files

--- waybar/modules/control-center.sh
+++
@@ -1,9 +0,0 @@
-
-# Default return
-CONTROL_CENTER_JSON="{ \"text\": \"\", \"bluetooth\": \"$BLUETOOTH_ACTIVE\" }"
-$BLUETOOTH_ACTIVE=false
-
-if [[ $(systemctl is-active bluetooth) =~ "active" ]]
-then
-    $BLUETOOTH_ACTIVE=true
-fi
