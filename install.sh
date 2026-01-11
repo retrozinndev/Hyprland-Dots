5582e8e796cda2ef0424307e4fa655c0ef79da6d
retrozinndev <joaovodias@gmail.com>
2026-01-11 13:48:09 -0300

    :wrench: chore(install): use colorshell installer and utils script internally

--- install.sh
+++ install.sh
@@ -8,12 +8,11 @@ trap "printf \"\nOh noo!! Some application just killed the script!\"; exit 2" SI
 XDG_CONFIG_HOME=`[[ -z "$XDG_CONFIG_HOME" ]] && echo "$HOME/.config" || echo "$XDG_CONFIG_HOME"`
 
 function Backup_previous() {
-    echo "Would you like to make a backup of the current configuration? [y/n] "
-    read answer
+    Ask "Would you like to make a backup of the current configuration?"
     if [[ $answer =~ ^y.*$ ]]; then
         sh ./backup-dots.sh
     else
-        Send_log "Skipping backup"
+        Send_log "backup skipped"
     fi
 }
 
@@ -25,7 +24,7 @@ Print_header
 echo -e "https://github.com/retrozinndev/Hyprland-Dots\n"
 sleep .5
 
-echo "Welcome to my dotfiles installation script!"
+echo "Welcome to retrozinndev's dotfiles installation script!"
 
 # Warn user of possible problems that can happen
 echo "!!!WARNING!!! By running this script, you assume total responsability for any issues that may occur with your filesystem"
@@ -33,43 +32,35 @@ echo "!!!WARNING!!! By running this script, you assume total responsability for 
 echo -n "Do you want to start the dotfiles installation? [y/n] "
 read input
 
-if [[ $input =~ ^y.*$ ]]; then
-    Backup_previous
-    Send_log "Starting installation...\n"
-
-    [[ -d ./colorshell ]] && \
-        git clone https://github.com/retrozinndev/colorshell.git ./colorshell \
-    || Send_log "previous colorshell clone found"
-
-    cd ./colorshell && git fetch && git pull \
-        || (Send_log err "couldn't fetch/pull from remote. try deleting the colorshell \
-directory and restarting installation" && exit 1)
-
-    sh install.sh -y && cd ..
-
-    for dir in ${config_dirs[@]}; do
-        dest=$XDG_CONFIG_HOME/$dir
-
-        echo "-> Installing $dir in $dest"
-        mkdir -p "$dest" # create parents
-
-        if [[ -f "./$dir" ]]; then
-            rm -r "$dest" # delete unused directory
-            cp -f ./$dir "$dest" # copy actual file
-        else
-            cp -rf ./$dir/* "$dest" # force-copy content
-        fi
-    done
-
-    Send_log "cleaning..."
-    rm -r ./colorshell
-
-    echo "Ah yes! Looks like it finished, yay :3"
-    echo -e "If you find any issue, please report it in: https://github.com/retrozinndev/Hyprland-Dots/issues"
-	echo "Thanks for using my dots and colorshell! I really appreciate that :D"
-
-    printf "\n"
-else
+if ! [[ $input =~ ^y(es)?$ ]]; then
     printf "Ok, doing as you said! Bye bye!\n"
-	exit 0
+    exit 0
 fi
+
+bash <(curl -s https://raw.githubusercontent.com/retrozinndev/colorshell/refs/heads/ryo/install.sh) -y
+
+source ${XDG_CACHE_HOME:-"$HOME/.cache"}/colorshell-installer/utils.sh
+
+
+for dir in ${config_dirs[@]}; do
+    dest=$XDG_CONFIG_HOME/$dir
+
+    Send_log info "-> Installing $dir in $dest"
+    mkdir -p `dirname $dest` # create parents
+
+    if [[ -f "./$dir" ]]; then
+        cp -rf ./$dir "$dest" # copy dir/file
+        continue
+    fi
+
+    if [[ -d "./$dir" ]]; then
+        cp -rf ./$dir/* $dest
+        continue
+    fi
+
+    Send_log warn "config \`$dir\` was skipped: not found / not accessible"
+done
+
+echo "Ah yes! Looks like it finished, yipee :D"
+echo -e "If you find any issue, please report it: https://github.com/retrozinndev/Hyprland-Dots/issues"
+    echo "Thanks for using colorshell + my dotfiles! I really appreciate it :P\n"
